ac4b9929df7de1f5cb3b4876a65ae13b4e0bdc63
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-03-30 19:45:06 -0600

    Need help with render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -17,19 +17,54 @@ class Cell
   end
 
   def fired_upon?
+    # PSEUDOCODE
+    # if @ship is value && @coordinate = coordinate
+      # ship has been hit & fired upon
+    #elsif @ship = nil && @coordinate = coordinate
+      #ship has been fired upon & it's a miss
+
     return false if @ship.health == ship.length
     true
   end
 
   def fire_upon
-    ship.hit
-  end
-
-  def render
-    if @ship == nil && @coordinate != coordinate
-      "."
-    elsif @ship == nil && @coordinate 
-      "M"
+    self.fired_upon? == true
+    if empty? == false
+      ship.hit
     end
   end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+  # def render(reveal_ships = nil)
+  #   if show_ships == true && empty? == false
+  #     "S"
+  #   elsif fired_upon? == false
+  #     "."
+  #   elsif fired_upon? == true && empty? == true
+  #     "M"
+  #   elsif fired_upon? == true && ship.sunk? == true
+  #     "X"
+  #   elsif fired_upon? == true && empty? == false
+  #     "H"
+  #   end
+  # end
+  #
+  # def render
+  #   if @ship == nil && @coordinate != coordinate
+  #     "."
+  #   elsif @ship == nil && @coordinate
+  #     "M"
+  #   end
+  # end
+  #
