0e6d19ce8f9981ad3b6142e1a52c3f3038cd0c5d
JoannaCoPo <jococopo@gmail.com>
2021-04-01 12:09:39 -0600

    Create valid_coordinate? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,5 +17,12 @@ RSpec.describe 'Board' do
     expect(board.cells.count).to eq(16)
   end
 
-
+  it 'validates coordinates' do
+    board = Board.new
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("D4")).to eq(true)
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
+  end
 end
