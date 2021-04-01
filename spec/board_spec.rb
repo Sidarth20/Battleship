6f76eee396fead7bc0bf208157e561b9d36da433
JoannaCoPo <jococopo@gmail.com>
2021-04-01 16:41:30 -0600

    Continue working on valid_placement? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -26,12 +26,20 @@ RSpec.describe 'Board' do
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
-  it 'validates placements' do
+it 'validates placements' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  xit 'validates placements are consecutive' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "C1"])).to eq(false)
   end
 
 
