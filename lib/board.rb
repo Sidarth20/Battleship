edb4c03b1bfd5d4d2f1ef3917e92a224453fb7c3
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-04-03 13:16:35 -0600

    Finish placing ships method

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,11 @@
 class Board
+  attr_reader :cells
 
   def initialize
-    @cells = cells
+    @cells = grid
   end
 
-  def cells
+  def grid
     {
       "A1" =>  Cell.new("A1"), "A2" =>  Cell.new("A2"),"A3" =>  Cell.new("A3"),"A4" =>  Cell.new("A4"),"B1" =>  Cell.new("B1"),
       "B2" =>  Cell.new("B2"),"B3" =>  Cell.new("B3"),"B4" =>  Cell.new("B4"),"C1" =>  Cell.new("C1"),  "C2" =>  Cell.new("C2"),
@@ -53,6 +54,7 @@ class Board
       end
     end
     values
+    # binding.pry
   end
 
 end
