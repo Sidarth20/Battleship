5f498d1d5164693ead3b3afdf1619034d9b29911
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-04-04 15:47:37 -0600

    Attempt at placing ship for computer

--- gameflow.rb
+++ gameflow.rb
@@ -12,17 +12,41 @@ require 'pry'
     user_input = gets.chomp
     if user_input == 'p'
       puts "May the odds be ever in your favor."
+    # elsif user_input == "\n"
+    #   puts "Please enter p or q"
     else
       puts "Have a good day!"
     end
+  # end
+
+  # def setup
+    board_computer = Board.new
+    cruiser_computer = Ship.new("Cruiser", 3)
+    submarine_computer = Ship.new("Submarine", 2)
+
+    puts "computer time"
+    #computer_setup
+    #need to generate random coordinates based on grid
+    # with adhering to valid placement principles
+    #^store above coordinates in computer_coordinates
+    #then
+    binding.pry
+    keys = board_computer.cells.keys
+    keys.sample(cruiser_computer.length, random: Random.new(1)) do |key|
+    until board_computer.valid_placement?(cruiser_computer, location)
+      new_placement = board_computer.place(cruiser_computer, key)
+    end
+
   end
 
   welcome_screen
-  #if welcome_screen's user_input == 'p'
+  # if user_input == 'p'
+  #   setup
+  # else
+  #   welcome_screen
+  # end
   #go into setup method
 
-  def setup
-    
-  end
+
 
 # end
