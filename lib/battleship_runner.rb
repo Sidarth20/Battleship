fcd3579cf00b8d668dbe73dd625ece17c4df2bbf
JoannaCoPo <jococopo@gmail.com>
2021-04-07 03:54:46 -0600

    Remove comments

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -30,23 +30,10 @@ require 'pry'
 
 
 def start_game_setup
-  # @cruiser_computer
-  # @submarine_computer
-  # @cruiser_player
-  # @submarine_player
-  # @board_computer
-  # @board_player
-  # @new_game.initialize(@comp_board = Board.new,
-                            # @player_board = Board.new,
-                            # @comp_cruiser = Ship.new("Cruiser", 3),
-                            # @comp_sub = Ship.new("Submarine", 2),
-                            # @player_cruiser = Ship.new("Cruiser", 3),
-                            # @player_sub = Ship.new("Submarine", 2))
   @new_game.welcome_screen
   @new_game.computer_turn_setup(@comp_cruiser, location = @new_placement)
   @new_game.player_instructions
   @new_game.display_boards
-  # @new_game.viable_ship_placment
 end
 
 def start_turns
